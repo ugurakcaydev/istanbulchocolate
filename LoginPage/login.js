7ddbb2994d6ac1ef2b5a041a62aa8d050b50cb93
Ugur Akcay <akcayugur01@gmail.com>
2023-03-19 19:46:59 +0300

    aa

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -5,10 +5,10 @@ function showPassword(id, el) {
     let x = document.getElementById(id)
     if (x.type == "password") {
         x.type = "text";
-        el.src = "./icons/1.png"
+        el.src = "/icons/1.png"
     } else {
         x.type = "password";
-        el.src = "./icons/2.png"
+        el.src = "/icons/2.png"
     }
 }
 
--- RegisterPage/register.js
+++ RegisterPage/register.js
@@ -2,9 +2,9 @@ function showPassword(id, el) {
     let x = document.getElementById(id)
     if (x.type == "password") {
         x.type = "text";
-        el.src = "../LoginPage/icons/1.png"
+        el.src = "/icons/1.png"
     } else {
         x.type = "password";
-        el.src = "../LoginPage/icons/2.png"
+        el.src = "/icons/2.png"
     }
 }
