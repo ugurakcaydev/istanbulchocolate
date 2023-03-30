03ee10d303912f013e30b1dc2fc9a25f2ffa59e2
Ugur Akcay <akcayugur01@gmail.com>
2023-03-30 04:18:13 +0300

    home changed

---
+++ HomePage/home.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  
+})
