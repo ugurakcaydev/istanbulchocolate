e426bf0914c92700be7566f303cc23dd9dcf54e0
Ugur Akcay <akcayugur01@gmail.com>
2023-05-03 21:15:59 +0300

    aa

--- BasketPage/basket.js
+++ BasketPage/basket.js
@@ -2,3 +2,22 @@ let storedProductNames = localStorage.getItem("productNames")
 
 let productNames = storedProductNames ? JSON.parse(storedProductNames) : [];
 console.log(productNames);
+
+
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
